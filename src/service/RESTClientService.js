e26b16c0f089a3a2a1391fd8abdd780958555603
Mohd Shariq <mdshariq614@gmail.com>
2022-07-02 23:45:30 +0530

    add config.js

--- src/service/RESTClientService.js
+++ src/service/RESTClientService.js
@@ -1,19 +1,20 @@
 import axios from 'axios';
+import { config } from './config';
 class RESTClientService{
     addOrUpdateServer(server){
-        return axios.put('http://localhost:8080/api/servers',server);
+        return axios.put(`http://${config.host}:${config.port}/api/servers`,server);
     }
     findAllServer(){
-       return axios.get('http://localhost:8080/api/servers');
+       return axios.get(`http://${config.host}:${config.port}/api/servers`);
     }
     findServertById(id){
-    return   axios.get(`http://localhost:8080/api/servers/${id}`);
+    return   axios.get(`http://${config.host}:${config.port}/api/servers/${id}`);
     }
     deleteServer(id){
-      return   axios.delete(`http://localhost:8080/api/servers/${id}`);
+      return   axios.delete(`http://${config.host}:${config.port}/api/servers/${id}`);
     }
     findServerByName(name){
-    return   axios.get(`http://localhost:8080/api/servers/find?name=${name}`);
+    return   axios.get(`http://${config.host}:${config.port}/api/servers/find?name=${name}`);
 
   }
 
---
+++ src/service/config.js
@@ -0,0 +1,4 @@
+export const config = {
+    host : "20.62.53.148",
+    port : "8080"
+};
